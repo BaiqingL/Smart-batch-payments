e971f613cb308ae62668a221b246b37d8e05629f
Baiqing Lyu <baiqinglyu@gmail.com>
2020-02-10 21:48:38 -0500

    Further optimizations

--- paymentContract.sol
+++ paymentContract.sol
@@ -48,30 +48,44 @@ contract paymentChannel{
         _;
     }
     
-    // OpenZepplin SafeMath.sol
-    function safeAdd(uint64 a, uint64 b) internal pure returns (uint64) {
+    // Modified slightly from OpenZepplin SafeMath
+    function safeAdd(
+        uint64 a, 
+        uint64 b
+    ) 
+        internal 
+        pure 
+        returns (uint64) 
+    {
         uint64 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
+        require(c >= a);
         return c;
     }
     
-    function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a, "SafeMath: subtraction overflow");
+    function safeSub(
+        uint256 a, 
+        uint256 b
+    ) 
+        internal 
+        pure 
+        returns (uint256) 
+    {
+        require(b <= a);
         uint256 c = a - b;
 
         return c;
     }
     
-    function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
-        if (a == 0) {
-            return 0;
-        }
-
+    function safeMul(
+        uint256 a, 
+        uint256 b
+    ) 
+        internal 
+        pure 
+        returns (uint256) 
+    {
         uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
+        require(c > a);
 
         return c;
     }
